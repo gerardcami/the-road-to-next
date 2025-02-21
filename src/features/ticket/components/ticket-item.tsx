6e8c914850df3ee0f52d28d765df6c6390fda8c1
Gerard Camí <gerardcamigay@gmail.com>
2025-02-21 17:01:31 +0100

    Server Queries -> Caching

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,27 +1,43 @@
+import { Ticket } from "@prisma/client";
 import clsx from "clsx";
-import { LucideArrowUpRightFromSquare } from "lucide-react";
+import { LucideArrowUpRightFromSquare, LucideTrash } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ticketPath } from "@/paths";
 
+import { deleteTicket } from "../actions/delete-ticket";
 import { TICKET_ICONS } from "../constants";
-import { Ticket } from "../types";
 
 type TicketItemProps = {
   ticket: Ticket;
+  /* Use interferred types from the API */
+  /* ticket:
+    | Awaited<ReturnType<typeof getTickets>>[number]
+    | Awaited<ReturnType<typeof getTicket>>; */
   isDetail?: boolean;
 };
 
 const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
+  if (!ticket) return null;
+
   const detailButton = (
     <Button variant="outline" size="icon" asChild>
-      <Link href={ticketPath(ticket.id)}>
+      <Link prefetch href={ticketPath(ticket.id)}>
         <LucideArrowUpRightFromSquare className=" size-4" />
       </Link>
     </Button>
   );
+
+  const deleteButton = (
+    <form action={deleteTicket.bind(null, ticket.id)}>
+      <Button variant="outline" size="icon">
+        <LucideTrash className="size-4" />
+      </Button>
+    </form>
+  );
+
   return (
     <div
       className={clsx("w-full flex gap-x-1", {
@@ -45,9 +61,9 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
             {ticket.content}
           </span>
         </CardContent>
-        {isDetail ? null : (
-          <div className=" flex flex-col gap-y-1">{detailButton}</div>
-        )}
+        <div className="flex- flex-col gap-y-1">
+          {isDetail ? deleteButton : detailButton}
+        </div>
       </Card>
     </div>
   );
--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,13 +1,12 @@
-import { initialTickets } from "@/data";
+import { cache } from "react";
 
-import { Ticket } from "../types";
+import { prisma } from "@/lib/prisma";
 
-export const getTicket = async (id: string): Promise<Ticket | null> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  const maybeTicket = initialTickets.find((ticket) => ticket.id === id);
-
-  return new Promise((resolve) => {
-    resolve(maybeTicket || null);
+export const getTicket = cache(async (id: string) => {
+  console.log(id);
+  return await prisma.ticket.findUnique({
+    where: {
+      id,
+    },
   });
-};
+});
